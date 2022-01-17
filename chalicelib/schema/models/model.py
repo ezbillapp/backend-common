643e2ef6a20039fdc7824e270b9b17d26c6fd150
Moises Navarro <moisalejandro@gmail.com>
2022-01-17 17:49:02 -0600

    [REF]

--- chalicelib/schema/models/model.py
+++ chalicelib/schema/models/model.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.schema import Table
 
-from chalicelib.schema import meta
+from .. import meta
 
 Base: Any = declarative_base(metadata=meta)
 
