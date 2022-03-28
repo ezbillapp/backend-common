31a1d98b695530ac5f1a7efb769bfb2c807c1aed
Moises Navarro <moisalejandro@gmail.com>
2022-03-28 15:57:06 -0600

    [WIP] export

--- chalicelib/controllers/common.py
+++ chalicelib/controllers/common.py
@@ -642,7 +642,6 @@ class CommonController:
         """Return a ZIP with the XML's of the records"""
         # if not records:
         #     raise NotFoundError("No records found")
-        session.add_all(records)
         _logger.info("Creating PDF, Records: %s", len(records))
         f = io.BytesIO()
         with ZipFile(f, "w") as zf:
