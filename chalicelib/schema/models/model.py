8a35c2dc51c198ee157feab99af784d95668c62e
Moises Navarro <moisalejandro@gmail.com>
2022-09-16 11:11:15 -0500

    [ADD] IdentifiedModel

--- chalicelib/schema/models/model.py
+++ chalicelib/schema/models/model.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.schema import Table
 
+from chalicelib.new.shared.domain.primitives import Identifier, identifier_default_factory
 from chalicelib.new.shared.infra.primitives import IdentifierORM
 
 from .. import meta
@@ -26,8 +27,19 @@ class BasicModel(Base):
     )
 
 
+class IdentifiedModel(BasicModel):
+    __abstract__ = True
+    __table__: Table
+
+    identifier = Column(
+        IdentifierORM(),
+        primary_key=True,
+        default=identifier_default_factory,
+    )
+
+
 class Model(BasicModel):
-    """Base model for all the models to be peristed in the database"""
+    """Base model for all the models to be persisted in the database"""
 
     __abstract__ = True
     __table__: Table
